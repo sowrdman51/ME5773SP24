42c1b091f4452996ccb5db1ed4c75ecd273df84a
James Smith <james.smith3@my.utsa.edu>
2024-02-07 17:02:52 -0600

    Included ChatGPT credit on fact.sh -James

--- assignment3/twice.sh
+++ assignment3/twice.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# All credit for this code goes to ChatGPT 3.5. The grade will go to Pratik and James! :)
+
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <N>"
