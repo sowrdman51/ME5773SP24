ed6335188a7ca4936768eb67cc1aabfca1d39ccf
Pratik Mitra <pratik.mitra@my.utsa.edu>
2024-02-27 15:23:10 -0600

    Q1

---
+++ assignment5/question_2/loadFiles.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------#
+#
+# This code was generate by Pratik, Smith, and ChatGPT.
+# Date: 02-27-2024
+# 2. Reading information from files
+#
+#----------------------------------------------------------------#
+#
+# a. Create a python script file named ‘loadFiles.py’.
+# b. Import the libraries time, numpy and h5py.
+#
+#----------------------------------------------------------------#
+#
+import time
+import numpy as np
+import h5py
+#
+#----------------------------------------------------------------#
+
